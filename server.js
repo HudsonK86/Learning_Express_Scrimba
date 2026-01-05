1723bd8bb0ebba1bec59d6ff2dc6d8ffe6a5a031
HudsonK86 <hudsonnguyen86@gmail.com>
2026-01-05 14:38:07 +0800

    Add API endpoint to filter startups based on query parameters in server.js

--- server.js
+++ server.js
@@ -1,6 +1,35 @@
 import express from "express"
+import { startups } from "./data/data.js"
 
+const PORT = 8000
 const app = express()
-const port = 8000
 
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+app.get("/api", (req, res) => {
+    let filteredStartups = startups
+
+    const { country, industry, continent, is_seeking_funding, has_mvp } = req.query
+
+    if (country) {
+        filteredStartups = filteredStartups.filter(startup => startup.country.toLowerCase() === country.toLowerCase())
+    }
+
+    if (industry) {
+        filteredStartups = filteredStartups.filter(startup => startup.industry.toLowerCase() === industry.toLowerCase())
+    }
+    
+    if (continent) {
+        filteredStartups = filteredStartups.filter(startup => startup.continent.toLowerCase() === continent.toLowerCase())
+    }
+    
+    if (is_seeking_funding) {
+        filteredStartups = filteredStartups.filter(startup => startup.is_seeking_funding === JSON.parse(is_seeking_funding.toLowerCase()))
+    }
+
+    if (has_mvp) {
+        filteredStartups = filteredStartups.filter(startup => startup.has_mvp === JSON.parse(has_mvp.toLowerCase()))
+    }
+    
+    res.json(filteredStartups)
+})
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
